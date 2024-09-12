79b432b0ccb388072c5c1401cad5fdea4509a195
taoran <taoran0796@163.com>
2024-09-12 17:28:25 +0800

    asdf

--- app/Avenue/Service/AvenueGoogleAuthService.php
+++ app/Avenue/Service/AvenueGoogleAuthService.php
@@ -51,22 +51,18 @@ class AvenueGoogleAuthService extends AbstractService
 //        Log::get()->info('access_token:' . json_encode($accessToken));
 
         //获取用户信息
-//        $accessToken = $res['access_token'] ?? '';
-        $oauthService = $this->client->getOAuth2Service();
-        $oauthService->setIdToken($res['id_token']);
-        $info = $oauthService->verifyIdToken();
-        Log::get()->info("id token解析: " . json_encode($info));
+        $data = $this->tokenInfo($res['id_token'] ?? '');
+        Log::get()->info("id token解析: " . json_encode($data));
         //保存数据库
 
         Log::get()->info('======== google notiify end ======');
     }
 
-    public function tokenInfo()
+    public function tokenInfo($idToken)
     {
-        $idToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjkzOTc3MWE3ODAwYzQxM2Y5MDA1MTAxMmQ5NzU5ODE5MTZkNzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxMjU0MDEyMjMzMy1uOXY0OWl0M2ZocHVldHY5dXNqMDA5YTRhbDNyMWM2Ni5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjEyNTQwMTIyMzMzLW45djQ5aXQzZmhwdWV0djl1c2owMDlhNGFsM3IxYzY2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTExNjA1NzM3NzM5NTg4NDQ2NTEyIiwiZW1haWwiOiJ0YW9yYW4xNDAxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoib1Q0a292ZDZCVHBrdmpwRkxoektGdyIsIm5hbWUiOiJ0YW8gdGFvIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0k2dmp2WFNwRW9MVHYxMy1Sd0RxOVIxOFR5ZTVuSF9kWE9CeGx5djZwSmdFYjUydz1zOTYtYyIsImdpdmVuX25hbWUiOiJ0YW8iLCJmYW1pbHlfbmFtZSI6InRhbyIsImlhdCI6MTcyNjEzMDcwMywiZXhwIjoxNzI2MTM0MzAzfQ.GnesEION30aBWavp2M5e3LOAkX-kPUMFv3IlXWbUEAfhZ_cnG3noCTv_MbRRRtJ3aBgATqdPPAEzGsrsq4wCtFJnTc2aeYmLen5EB55kf1h9IEbqQYHK5KdsJ-Ft4h9B2Aw5lI542BBu2B9oUOMBRM4NXaCDdDwvV5ReojI5vjE96SmfDLvJzValB-Fo77OlGfhRkFR1Px4hTqMKVYH41Rfshv6oxkf0Th60mA2rXcqtQuuV_VqoryRWIP5e30LopL_eaGfWB8kbMYxbgnDXt-HQPNuN0-6COezm4LbJ1uZoRMtH2LIVSuocVHC2SE5BVgK3qa3ygWcVPh1QZJ4w8g';
+        //$idToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjkzOTc3MWE3ODAwYzQxM2Y5MDA1MTAxMmQ5NzU5ODE5MTZkNzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxMjU0MDEyMjMzMy1uOXY0OWl0M2ZocHVldHY5dXNqMDA5YTRhbDNyMWM2Ni5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjEyNTQwMTIyMzMzLW45djQ5aXQzZmhwdWV0djl1c2owMDlhNGFsM3IxYzY2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTExNjA1NzM3NzM5NTg4NDQ2NTEyIiwiZW1haWwiOiJ0YW9yYW4xNDAxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoib1Q0a292ZDZCVHBrdmpwRkxoektGdyIsIm5hbWUiOiJ0YW8gdGFvIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0k2dmp2WFNwRW9MVHYxMy1Sd0RxOVIxOFR5ZTVuSF9kWE9CeGx5djZwSmdFYjUydz1zOTYtYyIsImdpdmVuX25hbWUiOiJ0YW8iLCJmYW1pbHlfbmFtZSI6InRhbyIsImlhdCI6MTcyNjEzMDcwMywiZXhwIjoxNzI2MTM0MzAzfQ.GnesEION30aBWavp2M5e3LOAkX-kPUMFv3IlXWbUEAfhZ_cnG3noCTv_MbRRRtJ3aBgATqdPPAEzGsrsq4wCtFJnTc2aeYmLen5EB55kf1h9IEbqQYHK5KdsJ-Ft4h9B2Aw5lI542BBu2B9oUOMBRM4NXaCDdDwvV5ReojI5vjE96SmfDLvJzValB-Fo77OlGfhRkFR1Px4hTqMKVYH41Rfshv6oxkf0Th60mA2rXcqtQuuV_VqoryRWIP5e30LopL_eaGfWB8kbMYxbgnDXt-HQPNuN0-6COezm4LbJ1uZoRMtH2LIVSuocVHC2SE5BVgK3qa3ygWcVPh1QZJ4w8g';
         $url = self::TOKENINFO_URL . '?id_token=' . $idToken;
         $data = Utils::httpRequest($url);
-
-        Log::get()->info("id token解析: " . json_encode($data));
+        return $data;
     }
 }
