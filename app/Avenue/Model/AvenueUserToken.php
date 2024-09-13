fad08f7aefbbadea518a4cee07d897a48e683624
taoran <taoran0796@163.com>
2024-09-13 11:24:18 +0800

    adf

---
+++ app/Avenue/Model/AvenueUserToken.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Avenue\Model;
+
+use Hyperf\Database\Model\SoftDeletes;
+use Mine\MineModel;
+
+class AvenueUserToken extends MineModel
+{
+    use SoftDeletes;
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'avenue_user_token';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected array $fillable = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [];
+}
--- app/Avenue/Service/AvenueGoogleAuthService.php
+++ app/Avenue/Service/AvenueGoogleAuthService.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 
 namespace App\Avenue\Service;
 
+use App\Avenue\Model\AvenueUser;
+use App\Avenue\Model\AvenueUserToken;
 use App\Package\Log;
 use App\Package\Utils;
-use Firebase\JWT\JWT;
+use App\Package\Verify;
+use Hyperf\DbConnection\Annotation\Transactional;
 use Hyperf\Di\Annotation\Inject;
 use Mine\Abstracts\AbstractService;
 use Google;
@@ -14,6 +17,8 @@ class AvenueGoogleAuthService extends AbstractService
 {
     #[Inject]
     public Google\Client $client;
+
+    protected \Xmo\JWTAuth\JWT $jwt;
 
     const TOKENINFO_URL = 'https://www.googleapis.com/oauth2/v3/tokeninfo';
 
@@ -29,38 +34,64 @@ class AvenueGoogleAuthService extends AbstractService
         return $url;
     }
 
+    #[Transactional]
     public function notify($params)
     {
-        Log::get()->info('========google notiify start ======');
+        Log::get()->info('========google notify start ======');
         Log::get()->info('参数：' . json_encode($params));
         if (!isset($params['code'])) {
             Log::get()->info('error: no code');
             return false;
         }
-//        $this->client->setAuthConfigFile();
-//        $this->client->setAuthConfig();
+
         $this->client->setClientId(config('google.client_id'));
         $this->client->setScopes('email profile');
         $this->client->setClientSecret(config('google.client_secret'));
         $this->client->setRedirectUri(config('google.grant_domain') . '/api/v1/avenue/google/auth/notify');
         $res = $this->client->fetchAccessTokenWithAuthCode($params['code']);
+
         Log::get()->info('auth code:' . json_encode($res));
 
-//        $accessToken = $this->client->getAccessToken();
-//
-//        Log::get()->info('access_token:' . json_encode($accessToken));
-
         //获取用户信息
-        $data = $this->tokenInfo($res['id_token'] ?? '');
-        Log::get()->info("id token解析: " . json_encode($data));
-        //保存数据库
+        $infoJson = $this->tokenInfo($res['id_token'] ?? '');
+        $info = json_decode($infoJson, true);
 
-        Log::get()->info('======== google notiify end ======');
+        //保存用户信息
+        $userInfo = make(AvenueUser::class)->where('email', $info['email'])->first();
+        $saveData = Utils::setSaveData([
+            'nick_name' => $info['name'],
+            'email' => $info['email'],
+            'avatar' => $info['picture'],
+        ]);
+        if ($userInfo) {
+            $userInfo->save($saveData);
+        } else {
+            $userInfo = make(AvenueUser::class)->save($saveData);
+        }
+
+        //生成并保存token
+        $token = $this->jwt->getToken([
+            'user_id' => $userInfo->id,
+            'email' => $userInfo->email,
+            'nick_name' => $userInfo->nick_name,
+        ]);
+        $userTokenSaveData = Utils::setSaveData([
+            'user_id' => $userInfo->id,
+            'token' => $token,
+        ]);
+        $userTokenInfo = make(AvenueUserToken::class)->where('user_id', $userInfo->id)->first();
+        if ($userTokenInfo) {
+            $userTokenInfo->save($userTokenSaveData);
+        } else {
+            make(AvenueUserToken::class)->save($userTokenSaveData);
+        }
+
+        Log::get()->info('======== google notify end ======');
+        header('Location: ' . config('google.grant_domain'));
     }
 
     public function tokenInfo($idToken)
     {
-        //$idToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjkzOTc3MWE3ODAwYzQxM2Y5MDA1MTAxMmQ5NzU5ODE5MTZkNzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxMjU0MDEyMjMzMy1uOXY0OWl0M2ZocHVldHY5dXNqMDA5YTRhbDNyMWM2Ni5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjEyNTQwMTIyMzMzLW45djQ5aXQzZmhwdWV0djl1c2owMDlhNGFsM3IxYzY2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTExNjA1NzM3NzM5NTg4NDQ2NTEyIiwiZW1haWwiOiJ0YW9yYW4xNDAxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoib1Q0a292ZDZCVHBrdmpwRkxoektGdyIsIm5hbWUiOiJ0YW8gdGFvIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0k2dmp2WFNwRW9MVHYxMy1Sd0RxOVIxOFR5ZTVuSF9kWE9CeGx5djZwSmdFYjUydz1zOTYtYyIsImdpdmVuX25hbWUiOiJ0YW8iLCJmYW1pbHlfbmFtZSI6InRhbyIsImlhdCI6MTcyNjEzMDcwMywiZXhwIjoxNzI2MTM0MzAzfQ.GnesEION30aBWavp2M5e3LOAkX-kPUMFv3IlXWbUEAfhZ_cnG3noCTv_MbRRRtJ3aBgATqdPPAEzGsrsq4wCtFJnTc2aeYmLen5EB55kf1h9IEbqQYHK5KdsJ-Ft4h9B2Aw5lI542BBu2B9oUOMBRM4NXaCDdDwvV5ReojI5vjE96SmfDLvJzValB-Fo77OlGfhRkFR1Px4hTqMKVYH41Rfshv6oxkf0Th60mA2rXcqtQuuV_VqoryRWIP5e30LopL_eaGfWB8kbMYxbgnDXt-HQPNuN0-6COezm4LbJ1uZoRMtH2LIVSuocVHC2SE5BVgK3qa3ygWcVPh1QZJ4w8g';
         $url = self::TOKENINFO_URL . '?id_token=' . $idToken;
         $data = Utils::httpRequest($url);
         return $data;
